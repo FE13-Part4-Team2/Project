ed8847c5b9487c0bd5e92c86a36d940fc4c1bde1
hyeonjiroh <sonamu6509@naver.com>
2025-04-24 23:28:12 +0900

    feat: ArticleComment API 함수 생성

---
+++ src/lib/apis/articleComment/type.ts
@@ -0,0 +1,23 @@
+import { UserResponse } from '@/lib/apis/user/type';
+
+export interface ArticleCommentBody {
+  content: string;
+}
+
+export interface ArticleCommentResponse {
+  id: number;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  writer: UserResponse;
+}
+
+export interface ArticleCommentListResponse {
+  list: ArticleCommentResponse[];
+  nextCursor: number | null;
+}
+
+export interface MessageResponse {
+  id: number;
+  message: string;
+}
