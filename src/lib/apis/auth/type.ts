57630f6887e50fe07bf38dc379208d8389147fb8
hyeonjiroh <sonamu6509@naver.com>
2025-04-24 19:29:03 +0900

    feat: Auth API 함수 생성

--- src/lib/apis/auth/type.ts
+++ src/lib/apis/auth/type.ts
@@ -1,18 +1,44 @@
-export interface LoginBody {
+interface User {
+  id: number;
+  nickname: string;
+  createdAt: string;
+  updatedAt: string;
+  image: string | null;
+  teamId: string;
   email: string;
-  password: string;
 }
 
-export interface LoginResponse {
-  user: {
-    id: number;
-    nickname: string;
-    createdAt: string;
-    updatedAt: string;
-    image: string | null;
-    teamId: string;
-    email: string;
-  };
+export interface OAuthAppBody {
+  appSecret: '클라이언트 id';
+  appKey: '';
+  provider: 'KAKAO';
+}
+
+export interface OAuthAppResponse {
+  id: number;
+  provider: 'KAKAO';
+  teamId: string;
+  appKey: '';
+  appSecret: '클라이언트 id';
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface OAuthBody {
+  state: string;
+  redirectUri: string;
+  token: string;
+}
+
+export interface AuthBody {
+  email: string;
+  nickname: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
+export interface AuthResponse {
+  user: User;
   accessToken: string;
   refreshToken: string;
 }
