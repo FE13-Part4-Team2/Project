d263995d1125f355e8faeb33bc00afd958b988fc
hyeonjiroh <sonamu6509@naver.com>
2025-04-25 23:45:54 +0900

    feat: 모달 상태에 props 추가

--- src/store/useModalStrore.ts
+++ src/store/useModalStrore.ts
@@ -1,14 +1,23 @@
 import { create } from 'zustand';
 import { ReactNode } from 'react';
 
-type ModalState = {
-  content: ReactNode | null;
-  openModal: (content: ReactNode) => void;
+interface ModalProps {
+  variant?: 'oneButton' | 'twoButton' | 'danger';
+  title?: string;
+  description?: string;
+  onSubmit?: () => void;
+}
+
+interface ModalState {
+  props: ModalProps;
+  content?: ReactNode | null;
+  openModal: (props: ModalProps, content?: ReactNode) => void;
   closeModal: () => void;
-};
+}
 
 export const useModalStore = create<ModalState>((set) => ({
+  props: {},
   content: null,
-  openModal: (content) => set({ content }),
-  closeModal: () => set({ content: null }),
+  openModal: (props, content) => set({ props, content }),
+  closeModal: () => set({ props: {}, content: null }),
 }));
