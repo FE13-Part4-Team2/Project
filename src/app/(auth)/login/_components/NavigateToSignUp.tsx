1ec32aca2c67cf0c2731e09a1d1aba302b547e3b
jjaneyxx <hmyeonge@gmail.com>
2025-05-01 15:46:58 +0900

    design: 가입 안내 문구, 가입하기 버튼 폰트 스타일 추가

--- src/app/(auth)/login/_components/NavigateToSignUp.tsx
+++ src/app/(auth)/login/_components/NavigateToSignUp.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 export default function NavigateToSignUp() {
   return (
     <div className="mt-6 flex justify-center gap-3">
-      <span>아직 계정이 없으신가요?</span>
-      <Link href={'/signup'} className="text-green-700">
+      <span className="leading=[19px] font-medium">
+        아직 계정이 없으신가요?
+      </span>
+      <Link href={'/signup'} className="font-medium text-emerald-500 underline">
         가입하기
       </Link>
     </div>
