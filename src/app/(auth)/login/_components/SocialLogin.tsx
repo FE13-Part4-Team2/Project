5a4aeab55802f9b728cbd5fbec0bc4a3c99dac06
jjaneyxx <hmyeonge@gmail.com>
2025-05-01 15:44:35 +0900

    design: SocialLogin 레이아웃 구현

--- src/app/(auth)/login/_components/SocialLogin.tsx
+++ src/app/(auth)/login/_components/SocialLogin.tsx
@@ -2,11 +2,26 @@ import Image from 'next/image';
 
 export default function SocialLogin() {
   return (
-    <div className="flex items-center justify-between">
-      <span>간편 로그인하기</span>
-      <button>
-        <Image src="/icon/kakao_icon.svg" alt="kakao" width={42} height={42} />
-      </button>
+    <div className="mt-12">
+      {/* line */}
+      <div className="mb-3 flex items-center gap-4">
+        <div className="flex-1 border-t border-white/20" />
+        <span>OR</span>
+        <div className="flex-1 border-t border-white/20" />
+      </div>
+
+      {/*social login*/}
+      <div className="flex items-center justify-between">
+        <span className="leading-[19px] font-medium">간편 로그인하기</span>
+        <button>
+          <Image
+            src="/icon/kakao_icon.svg"
+            alt="kakao"
+            width={42}
+            height={42}
+          />
+        </button>
+      </div>
     </div>
   );
 }
