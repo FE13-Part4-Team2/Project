7dc172d574633c571de66f06f9d6dcbd7ea6afa4
ss <tt@email.com>
2025-05-02 22:41:40 +0900

    chore: 디자인 조금 수정

--- src/components/common/Header/SideMenu.tsx
+++ src/components/common/Header/SideMenu.tsx
@@ -26,7 +26,7 @@ export default function SideMenu({
           <div
             className={`fixed top-0 left-0 z-50 h-full w-[250px] border border-slate-50/10 bg-[#1E293B] p-6 transition-transform duration-300 ${isSideMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
           >
-            <div className="mb-6 flex justify-end">
+            <div className="mb-8 flex justify-end">
               <button
                 type="button"
                 className="cursor-pointer hover:cursor-pointer"
@@ -36,7 +36,7 @@ export default function SideMenu({
               </button>
             </div>
 
-            <ul className="flex flex-col gap-6 text-sm font-normal text-white">
+            <ul className="flex flex-col gap-6 text-sm font-normal">
               {memberships.map(({ group, role }) => (
                 <li
                   key={group.id}
