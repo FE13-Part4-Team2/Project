c423af20fe4306fd6eb4d5319828b333c72b7f6f
MinJi Cho <jam96013@gmail.com>
2025-05-07 00:07:18 +0900

    [design] team: 팀 배너 UI 구현

--- src/app/(team)/team/[teamid]/page.tsx
+++ src/app/(team)/team/[teamid]/page.tsx
@@ -1,3 +1,9 @@
+import TeamBanner from '@/app/(team)/team/_components/TeamBanner';
+
 export default function TeamPage() {
-  return <div>TeamPage</div>;
+  return (
+    <div className="flex h-screen w-full flex-col items-center p-6">
+      <TeamBanner />
+    </div>
+  );
 }
--- src/app/(team)/team/_components/TeamBanner.tsx
+++ src/app/(team)/team/_components/TeamBanner.tsx
@@ -1,1 +1,32 @@
+import Image from 'next/image';
 import clsx from 'clsx';
+import IconRenderer from '@/components/common/Icons/IconRenderer';
+
+const title = '경영관리팀';
+
+const TeamBanner = () => {
+  return (
+    <div
+      className={clsx(
+        'relative flex items-center justify-between p-6',
+        'laptop:w-[1200px]',
+        'tablet:w-[696px]',
+        'h-[64px] w-[343px]',
+        'rounded-[12px] border border-slate-50/10 bg-[#272e3f]',
+        'text-xl-bold'
+      )}
+    >
+      <Image
+        src="/image/team_banner_pattern.svg"
+        alt="팀 배너 배경 패턴 이미지"
+        width={181}
+        height={64}
+        className="tablet:left-[75%] absolute left-1/2 -translate-x-1/2"
+      />
+      {title}
+      <IconRenderer name="GearIcon" />
+    </div>
+  );
+};
+
+export default TeamBanner;
