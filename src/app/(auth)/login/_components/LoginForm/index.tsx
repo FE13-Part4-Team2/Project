4e104a632154b3ef7849553cf4ad4f9eb0bed883
jjaneyxx <hmyeonge@gmail.com>
2025-05-07 13:03:29 +0900

    design: LoginForm, InputWithlabel 컴포넌트 초기화

--- src/app/(auth)/login/_components/LoginForm/index.tsx
+++ src/app/(auth)/login/_components/LoginForm/index.tsx
@@ -3,64 +3,11 @@
 import ForgotPasswordButton from '@/app/(auth)/login/_components/LoginForm/ForgotPasswordButton';
 import InputWithLabel from '@/app/(auth)/login/_components/LoginForm/InputWithLabel';
 import Button from '@/components/common/Button';
-import { signIn } from '@/lib/apis/auth';
-import { useState } from 'react';
-import { toast } from 'react-toastify';
 
 export default function LoginForm() {
-  const [isEmailValid, setIsEmailValid] = useState<boolean>(false);
-  const [isPasswordValid, setIsPasswordValid] = useState<boolean>(false);
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [formErrors, setFormErrors] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleOpenModal = () => {
-    // open password-find modal
-  };
-
-  // form submit
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (!isEmailValid || !isPasswordValid) return;
-
-    //
-    try {
-      const res = await signIn({
-        body: {
-          email,
-          password,
-        },
-      });
-      console.log(res);
-      toast.success('로그인 성공'); // test
-    } catch (error) {
-      if (error instanceof Error) {
-        // 비밀번호가 일치하지 않는 경우
-        if (error.message.includes('비밀번호')) {
-          setFormErrors({
-            email: '',
-            password: error.message,
-          });
-          return;
-        }
-        // 가입되지 않은 유저일 경우
-        if (error.message.includes('이메일')) {
-          setFormErrors({
-            email: error.message, // 존재하지 않는 이메일입니다.
-            password: '',
-          });
-          return;
-        }
-
-        // Network Error
-        console.error('Network Error', error);
-        toast.error('예상치 못한 오류가 발생했습니다. ');
-      }
-    }
+    // form submit logic here
   };
 
   return (
@@ -68,24 +15,11 @@ export default function LoginForm() {
       <h1 className="text-4xl-medium mb-20 text-center">로그인</h1>
 
       <div className="flex flex-col gap-6">
-        <InputWithLabel
-          inputType="email"
-          autoComplete="email"
-          onValidChange={setIsEmailValid}
-          onValueChange={setEmail}
-          submitErrorMessage={formErrors.email}
-        />
-
-        <InputWithLabel
-          inputType="password"
-          autoComplete="current-password"
-          onValidChange={setIsPasswordValid}
-          onValueChange={setPassword}
-          submitErrorMessage={formErrors.password}
-        />
+        <InputWithLabel inputType="email" />
+        <InputWithLabel inputType="password" />
       </div>
 
-      <ForgotPasswordButton onClick={handleOpenModal} />
+      <ForgotPasswordButton />
 
       <Button
         size="lg"
@@ -93,7 +27,7 @@ export default function LoginForm() {
         styleType="filled"
         radius="sm"
         className="w-[460px]"
-        disabled={!(isEmailValid && isPasswordValid)}
+        disabled={true} // 나중에 유효성 체크 후 해제
       >
         로그인
       </Button>
