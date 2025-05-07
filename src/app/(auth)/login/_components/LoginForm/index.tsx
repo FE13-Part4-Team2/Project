a33a5691f7045452ff2e2231e83a418d69bf8af0
jjaneyxx <hmyeonge@gmail.com>
2025-05-07 14:10:30 +0900

    feat: Zod schema 정의 및 에러 메시지 InputWithLabel 에 props 로 전달

--- src/app/(auth)/login/_components/LoginForm/index.tsx
+++ src/app/(auth)/login/_components/LoginForm/index.tsx
@@ -3,20 +3,63 @@
 import ForgotPasswordButton from '@/app/(auth)/login/_components/LoginForm/ForgotPasswordButton';
 import InputWithLabel from '@/app/(auth)/login/_components/LoginForm/InputWithLabel';
 import Button from '@/components/common/Button';
+import { validateEmail, validatePassword } from '@/utils/inputValidation';
+import { useState } from 'react';
+import { z } from 'zod';
 
 export default function LoginForm() {
+  const [formErrors, setFormErrors] = useState({
+    email: '',
+    password: '',
+  });
   const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // form submit logic here
   };
+
+  // auth schema
+  const auth = z.object({
+    email: z
+      .string()
+      .nonempty({ message: '이메일은 필수 입력입니다.' })
+      .refine(validateEmail, {
+        message: '올바른 이메일 형식이 아닙니다.',
+      }),
+
+    password: z
+      .string()
+      .nonempty({ message: '비밀번호는 필수 입력입니다.' })
+      .refine(validatePassword, {
+        message:
+          '비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.',
+      }),
+  });
+
+  // handling validation with Zod
+  const result = auth.safeParse({ email, password });
+
+  // 스키마의 필드에서 에러 메시지를 꺼내서 상태에 저장하고, UI 에 전달.
+  if (!result.success) {
+    // 실패 정보가 담긴 zodError 객체를 flatten( ) 으로 보기좋게 정리, 필드별 에러 메시지만 꺼냄
+    const fieldErrors = result.error.flatten().fieldErrors;
+    // 필드별 첫번째 에러 메시지를 꺼내서, 상태로 저장
+    setFormErrors({
+      email: fieldErrors.email?.[0] || '',
+      password: fieldErrors.password?.[0] || '',
+    });
+    return;
+  }
 
   return (
     <form onSubmit={handleFormSubmit}>
       <h1 className="text-4xl-medium mb-20 text-center">로그인</h1>
 
       <div className="flex flex-col gap-6">
-        <InputWithLabel inputType="email" />
-        <InputWithLabel inputType="password" />
+        <InputWithLabel inputType="email" errorMessage={formErrors.email} />
+        <InputWithLabel
+          inputType="password"
+          errorMessage={formErrors.password}
+        />
       </div>
 
       <ForgotPasswordButton />
