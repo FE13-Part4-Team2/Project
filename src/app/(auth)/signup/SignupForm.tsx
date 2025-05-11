3fa9ed61a19eabc3b4ff365ba21707922bd02dfa
jjaneyxx <hmyeonge@gmail.com>
2025-05-12 08:51:22 +0900

    refactor: 네트워크 오류 toast 메시지 개선

--- src/app/(auth)/signup/SignupForm.tsx
+++ src/app/(auth)/signup/SignupForm.tsx
@@ -199,9 +199,12 @@ export default function SignupForm() {
         }
 
         console.error(error);
-
-        // 그 외 오류
-        toast.error('알 수 없는 오류가 발생했습니다.');
+        toast.error(
+          <>
+            문제가 발생했습니다. 잠시 후 다시 시도해주세요.
+            <br />({errorMessage})
+          </>
+        );
       }
     }
   };
