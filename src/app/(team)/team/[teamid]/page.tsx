73c515a358d64698ada9e9467b7dc39eb3fd481f
MinJi Cho <jam96013@gmail.com>
2025-05-12 04:27:42 +0900

    [feat] team: TaskListBar api 연결

--- src/app/(team)/team/[teamid]/page.tsx
+++ src/app/(team)/team/[teamid]/page.tsx
@@ -7,22 +7,29 @@ import TaskListBar from '@/app/(team)/team/_components/TaskListBarList/TaskListB
 
 export default async function TeamPage({
   params,
+  searchParams,
 }: {
   params: { teamid: string };
+  searchParams: { taskListId: string; date: string };
 }) {
   const userId = cookies().get('userId')?.value;
   const groupId = Number(params.teamid);
-  const groupData = await getGroupById({ groupId });
-  const taskListsData = groupData?.taskLists ?? [];
 
-  if (!groupData) {
+  const groupData = await getGroupById({ groupId });
+  const taskListData = await getTaskListById({
+    taskListId: Number(searchParams.taskListId),
+    date: searchParams.date,
+    tag: ['task'],
+  });
+
+  if (!groupData || !taskListData) {
     notFound();
   }
 
   return (
     <div className="flex w-full flex-col items-center gap-6 p-6">
       <TeamBanner group={groupData} userId={Number(userId)} />
-      <TaskListBar />
+      <TaskListBar taskList={taskListData} />
     </div>
   );
 }
--- src/app/(team)/team/_components/TaskListBarList/TaskListBar.tsx
+++ src/app/(team)/team/_components/TaskListBarList/TaskListBar.tsx
@@ -1,19 +1,18 @@
 'use client';
 import ProcessBadge from '@/app/(team)/team/_components/TaskListBarList/ProcessBadge';
 import GradientScrollable from '@/components/common/Scroll/GradientScrollable';
+import { TaskListResponse } from '@/lib/apis/taskList/type';
 import {
   taskListBarWrapperStyle,
   taskListBarTitleStyle,
 } from '@/app/(team)/team/_components/styles';
 
-const TaskListBar = () => {
+const TaskListBar = ({ taskList }: { taskList: TaskListResponse }) => {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <div className={`${taskListBarWrapperStyle}`}>
         <GradientScrollable>
-          <p className={`${taskListBarTitleStyle}`}>
-            목록 이름 30자 제한이라서 500에러 발생하는 듯하다
-          </p>
+          <p className={`${taskListBarTitleStyle}`}>{taskList.name}</p>
         </GradientScrollable>
         <ProcessBadge />
       </div>
