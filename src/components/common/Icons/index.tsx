c53863e931d5d0a6c24ffcfb9f09384e0c6b531e
ss <tt@email.com>
2025-05-12 13:51:11 +0900

    feat: ProfileEdit 아이콘 추가

--- src/components/common/Icons/index.tsx
+++ src/components/common/Icons/index.tsx
@@ -22,6 +22,7 @@ import List from '@/assets/icons/list.svg';
 import Logo from '@/assets/icons/logo.svg';
 import Member from '@/assets/icons/member.svg';
 import Plus from '@/assets/icons/plus.svg';
+import ProfileEdit from '@/assets/icons/profile_edit.svg';
 import ProfileImage from '@/assets/icons/profile_image.svg';
 import ProgressDone from '@/assets/icons/progress_done.svg';
 import ProgressOngoing from '@/assets/icons/progress_ongoing.svg';
@@ -197,6 +198,15 @@ const Icons = {
 
   PlusIcon: ({ size = 24, className, onClick }: IconProps) => (
     <Plus width={size} height={size} className={className} onClick={onClick} />
+  ),
+
+  ProfileEditIcon: ({ size = 24, className, onClick }: IconProps) => (
+    <ProfileEdit
+      width={size}
+      height={size}
+      className={className}
+      onClick={onClick}
+    />
   ),
 
   ProfileImageIcon: ({ size = 24, className, onClick }: IconProps) => (
