dd8c2c746b3866a332aab74f03a78134a51e57b4
hyeonjiroh <sonamu6509@naver.com>
2025-05-12 16:30:20 +0900

    design: 커스텀 datepicker 헤더 스타일 변경

--- src/components/common/Datepicker/index.tsx
+++ src/components/common/Datepicker/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
+import { ko } from 'date-fns/locale';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export default function CustomDatePicker() {
@@ -10,8 +11,20 @@ export default function CustomDatePicker() {
       <DatePicker
         selected={startDate}
         onChange={(date) => date && setStartDate(date)}
-        dateFormat="yyyy-MM-dd"
+        locale={ko}
         inline
+        renderCustomHeader={({ date, decreaseMonth, increaseMonth }) => {
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          return (
+            <div className="text-md-medium flex items-center justify-between bg-slate-800 px-4 py-2 text-white">
+              <button onClick={decreaseMonth}>&lt;</button>
+              <span>{`${year}년 ${month}월 ${day}일`}</span>
+              <button onClick={increaseMonth}>&gt;</button>
+            </div>
+          );
+        }}
       />
     </div>
   );
