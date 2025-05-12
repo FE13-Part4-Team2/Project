af7b5909921a4e884d85ed4427bbcb401b6e718d
jjaneyxx <hmyeonge@gmail.com>
2025-05-12 09:18:06 +0900

    fix(SignupPage, LoginPage): 회원가입, 로그인 페이지 스크롤바 이슈 해결

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from '@/app/(auth)/signup/SignupForm';
 
 export default function SignupPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex h-[calc(100vh-60px)] items-center justify-center">
       <div className="flex flex-col">
         <SignupForm />
       </div>
