1b563da831bb0b02fb2665b26f060fe20b254469
hyeonjiroh <sonamu6509@naver.com>
2025-05-14 09:56:26 +0900

    feat: 특정 날짜의 할 일 목록 표시 기능 추가

--- src/app/(team)/team/[teamid]/tasklist/_components/DateMenu/CalendarButton.tsx
+++ src/app/(team)/team/[teamid]/tasklist/_components/DateMenu/CalendarButton.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import IconRenderer from '@/components/common/Icons/IconRenderer';
+import CustomDatePicker from '@/components/common/Datepicker';
 
 export default function CalendarButton() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const id = searchParams.get('id');
+  const currentDate = searchParams.get('date');
+
+  const [selectedDate, setSelectedDate] = useState(new Date(currentDate!));
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+
+  useEffect(() => {
+    const newDateString = selectedDate.toISOString().slice(0, 10);
+    const params = new URLSearchParams(searchParams);
+    params.set('id', id!);
+    params.set('date', newDateString);
+    router.push(`?${params.toString()}`);
+    setIsDatePickerOpen(false);
+  }, [selectedDate]);
+
   return (
-    <button
-      type="button"
-      className="flex size-6 items-center justify-center rounded-full bg-slate-800 transition-colors duration-100 hover:bg-slate-700"
-    >
-      <IconRenderer name="CalendarIcon" size={12} />
-    </button>
+    <div className="relative">
+      <button
+        type="button"
+        onClick={() => setIsDatePickerOpen((prev) => !prev)}
+        className="flex size-6 items-center justify-center rounded-full bg-slate-800 transition-colors duration-100 hover:bg-slate-700"
+      >
+        <IconRenderer name="CalendarIcon" size={12} />
+      </button>
+      {isDatePickerOpen && (
+        <div className="absolute top-7">
+          <CustomDatePicker
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+          />
+        </div>
+      )}
+    </div>
   );
 }
