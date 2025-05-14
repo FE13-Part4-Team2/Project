73e39fb8d80fb8007d0dbf62b847a01728077cd4
해명 <hmyeonge@gmail.com>
2025-05-15 07:26:58 +0900

    chore: 뷰포트 높이 계산 60px 로 수정

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -2,7 +2,7 @@ import ResetPasswordForm from '@/app/(auth)/reset-password/ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
-    <div className="flex h-[calc(100vh-80px)] justify-center pt-[140px]">
+    <div className="flex h-[calc(100vh-60px)] justify-center pt-[140px]">
       <div className="flex flex-col">
         <ResetPasswordForm />
       </div>
