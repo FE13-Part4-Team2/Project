32b179294d60dbfcff2d4e0feee84ebb8e43fc24
해명 <hmyeonge@gmail.com>
2025-05-14 13:21:53 +0900

    design: 비밀번호 재설정 기본 레이아웃 구성

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,3 +1,12 @@
+import ResetPasswordForm from "@/app/(auth)/reset-password/ResetPasswordForm";
+
+
 export default function ResetPasswordPage() {
-  return <div>ResetPasswordPage</div>;
+  return (
+    <div className="flex h-[calc(100vh-60px)] items-center justify-center">
+      <div className="flex flex-col">
+        <ResetPasswordForm />
+      </div>
+    </div>
+  );
 }
