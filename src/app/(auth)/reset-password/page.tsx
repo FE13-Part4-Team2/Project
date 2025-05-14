7b96c9acf65ad17bee3c82da781080f2412f51b8
해명 <hmyeonge@gmail.com>
2025-05-15 05:11:51 +0900

    feat: 비밀번호, 비밀번호 확인 필드 placeholder 문구 추가

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,9 +1,8 @@
-import ResetPasswordForm from "@/app/(auth)/reset-password/ResetPasswordForm";
-
+import ResetPasswordForm from '@/app/(auth)/reset-password/ResetPasswordForm';
 
 export default function ResetPasswordPage() {
   return (
-    <div className="flex h-[calc(100vh-60px)] items-center justify-center">
+    <div className="mt-[140px] flex h-[calc(100vh-60px)] justify-center">
       <div className="flex flex-col">
         <ResetPasswordForm />
       </div>
