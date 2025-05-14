6dc5df004ec53785fc6d8312828adde5d9fa3439
해명 <hmyeonge@gmail.com>
2025-05-15 06:06:53 +0900

    feat: schema 통합 및 유효성 검사에 따른 오류 메시지 표시

--- src/app/(auth)/reset-password/ResetPasswordForm.tsx
+++ src/app/(auth)/reset-password/ResetPasswordForm.tsx
@@ -2,22 +2,76 @@
 
 import InputWithLabel from '@/components/auth/InputWithLabel';
 import Button from '@/components/common/Button';
-import { useState } from 'react';
+import { validatePassword } from '@/utils/inputValidation';
+import { useMemo, useState } from 'react';
+import { z } from 'zod';
+
+// schema
+const resetPasswordSchema = z.object({
+  password: z
+    .string()
+    .nonempty({ message: '비밀번호는 필수 입력입니다.' })
+    .refine(validatePassword, {
+      message:
+        '비밀번호는 영문, 숫자, 특수문자를 포함한 8자 이상이어야 합니다.',
+    }),
+  passwordConfirm: z
+    .string()
+    .nonempty({ message: '비밀번호 확인은 필수 입력입니다.' }),
+});
 
 export default function ResetPasswordForm() {
-  const [formErrors, setFormErrors] = useState<{
-    email: string[] | undefined;
-    password: string[] | undefined;
+  const [formValues, setFormValues] = useState<{
+    password: string;
+    passwordConfirm: string;
   }>({
-    email: [],
-    password: [],
+    password: '',
+    passwordConfirm: '',
   });
+  const [formErrors, setFormErrors] = useState<{
+    password: string[] | undefined;
+    passwordConfirm: string[] | undefined;
+  }>({
+    password: [],
+    passwordConfirm: [],
+  });
+
+  const isFormValid = useMemo(() => {
+    return (
+      formValues.password !== '' &&
+      formValues.passwordConfirm !== '' &&
+      (formErrors.password?.length ?? 0) === 0 &&
+      (formErrors.passwordConfirm?.length ?? 0) === 0
+    );
+  }, [formValues, formErrors]);
+
+  const handleInputChange =
+    (key: 'password' | 'passwordConfirm') =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newFormValues = { ...formValues, [key]: e.target.value };
+      const result = resetPasswordSchema.safeParse(newFormValues);
+
+      if (!result.success) {
+        const fieldErrors = result.error.flatten().fieldErrors;
+
+        setFormErrors((prev) => ({
+          ...prev,
+          [key]: fieldErrors[key],
+        }));
+      } else {
+        setFormErrors((prev) => ({
+          ...prev,
+          [key]: '',
+        }));
+      }
+      setFormValues(newFormValues);
+    };
 
   const handleFormSubmit = () => {
     console.log('form submit');
     setFormErrors({
-      email: [],
       password: [],
+      passwordConfirm: [],
     });
   };
   return (
@@ -27,13 +81,15 @@ export default function ResetPasswordForm() {
       <div className="mb-10 flex flex-col gap-6">
         <InputWithLabel
           inputType="password"
-          errorMessage={formErrors.email}
+          errorMessage={formErrors.password}
           mode="resetPasswordPage"
+          onChange={handleInputChange('password')}
         />
         <InputWithLabel
           inputType="passwordConfirm"
-          errorMessage={formErrors.password}
+          errorMessage={formErrors.passwordConfirm}
           mode="resetPasswordPage"
+          onChange={handleInputChange('passwordConfirm')}
         />
       </div>
 
@@ -43,7 +99,7 @@ export default function ResetPasswordForm() {
         styleType="filled"
         radius="sm"
         className="w-[460px]"
-        disabled={false}
+        disabled={!isFormValid}
       >
         재설정
       </Button>
