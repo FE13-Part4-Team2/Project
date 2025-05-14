18a7eb2d509926cc2f78d4f3706174f684e9d23a
hyeonjiroh <sonamu6509@naver.com>
2025-05-14 23:00:01 +0900

    feat: 아티클 상세 섹션 데이터 API 연결 및 레이아웃

---
+++ src/app/(board)/article/[articleid]/ArticleDetailSection/index.tsx
@@ -0,0 +1,40 @@
+import { ArticleResponse } from '@/lib/apis/article/type';
+import WriterInfo from '@/components/user/WriterInfo';
+import DateInfo from '@/app/(board)/article/[articleid]/ArticleDetailSection/DateInfo';
+import CommentInfo from '@/app/(board)/article/[articleid]/ArticleDetailSection/CommentInfo';
+
+export default function ArticleDetailSection({
+  id,
+  title,
+  image,
+  createdAt,
+  writer,
+  content,
+  likeCount,
+  isLiked,
+  commentCount,
+}: ArticleResponse) {
+  return (
+    <div className="tablet:min-h-[312px] flex min-h-[242px] flex-col gap-6">
+      <div>
+        <div className="flex h-16 items-center justify-between border-b border-slate-50/10">
+          <div className="text-2lg-bold tablet:text-xl-bold">{title}</div>
+          <div>메뉴</div>
+        </div>
+        <div className="laptop:h-16 flex h-[72px] items-center justify-between">
+          <div className="flex gap-4">
+            {writer && <WriterInfo {...writer} />}
+            {createdAt && <DateInfo date={createdAt} />}
+          </div>
+          <div className="flex gap-4">
+            {commentCount !== undefined && (
+              <CommentInfo commentCount={commentCount} />
+            )}
+            <div>좋아요</div>
+          </div>
+        </div>
+      </div>
+      <div className="text-md-regular tablet:text-lg-medium">{content}</div>
+    </div>
+  );
+}
--- src/app/(board)/article/[articleid]/page.tsx
+++ src/app/(board)/article/[articleid]/page.tsx
@@ -1,3 +1,22 @@
-export default function ArticleDetailPage() {
-  return <div>ArticleDetailPage</div>;
+import { getArticleById } from '@/lib/apis/article';
+import ArticleDetailSection from '@/app/(board)/article/[articleid]/ArticleDetailSection';
+import ArticleCommentSection from '@/app/(board)/article/[articleid]/ArticleCommentSection';
+
+interface PageProps {
+  params: { articleid: string };
+}
+
+export default async function ArticleDetailPage({ params }: PageProps) {
+  const articleId = Number(params.articleid);
+
+  const articleData = await getArticleById({ articleId, tag: ['article'] });
+
+  return (
+    <div className="tablet:px-6 tablet:py-14 relative px-4 py-10">
+      <div className="m-auto flex max-w-[1200px] flex-col gap-10">
+        {articleData && <ArticleDetailSection {...articleData} />}
+        <ArticleCommentSection />
+      </div>
+    </div>
+  );
 }
