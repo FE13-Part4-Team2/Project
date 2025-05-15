fb44f8a10b004609542d2cf57f508b26db093613
MinJi Cho <jam96013@gmail.com>
2025-05-15 19:20:46 +0900

    [design] team: MemberCard & ReportBanner css 수정

--- src/app/(team)/team/_components/ReportBanner/index.tsx
+++ src/app/(team)/team/_components/ReportBanner/index.tsx
@@ -1,4 +1,6 @@
+'use client';
 import ReportCard from '@/app/(team)/team/_components/ReportBanner/ReportCard';
+import { useIsMobile } from '@/hooks/useCheckViewport';
 import {
   reportBannerContainerStyle,
   reportCardsWrapperStyle,
@@ -9,19 +11,36 @@ interface ReportBannerProps {
 }
 
 const ReportBanner = () => {
+  const isMobile = useIsMobile();
+
   return (
     <div className={`${reportBannerContainerStyle}`}>
       {/* 아이템 래퍼 */}
-      <div className="flex w-full items-center justify-between p-8">
+      <div className="tablet:p-8 flex w-full items-center justify-between p-3">
         {/* 진행률 */}
         <div className="flex items-center justify-center gap-10">
-          <div className="h-[140px] w-[140px] rounded-full border-[30px] border-green-700" />
-          <p>
-            오늘의
-            <br />
-            진행 상황
-          </p>
+          {/* 임시 원형 아이콘 */}
+          <div className="relative h-[140px] w-[140px] rounded-full border-[30px] border-green-700" />
+
+          <div className="tablet:relative absolute">
+            {isMobile ? (
+              '오늘'
+            ) : (
+              <>
+                오늘의
+                <br />
+                진행 상황
+              </>
+            )}
+            <p
+              className="tablet:text-[40px] text-[20px] font-bold"
+              style={{ backgroundImage: 'var(--green-gradient)' }}
+            >
+              25%
+            </p>
+          </div>
         </div>
+
         {/* Todo & Done Card */}
         <div className={`${reportCardsWrapperStyle}`}>
           <ReportCard variant="todo" />
