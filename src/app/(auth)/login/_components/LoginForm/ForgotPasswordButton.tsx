d8bfec0b1163efa7d143cebd1a0186000ebbad4c
해명 <hmyeonge@gmail.com>
2025-05-16 10:33:11 +0900

    feat: 비밀번호 재설정 모달 기본 동작 구현

--- src/app/(auth)/login/_components/LoginForm/ForgotPasswordButton.tsx
+++ src/app/(auth)/login/_components/LoginForm/ForgotPasswordButton.tsx
@@ -2,9 +2,36 @@
 
 import ResetPasswordLinkModal from '@/components/common/Modal/content/ResetPasswordLinkModal';
 import { useModalStore } from '@/store/useModalStore';
+import { validateEmail } from '@/utils/inputValidation';
+import { useEffect, useState } from 'react';
 
+interface ResetPasswordBodyType {
+  email: string;
+}
+
+// 클릭 시 비밀번호 재설정 모달 열림
+// openModal : options, content 를 받아서 모달을 렌더링 & store 에 상태 저장
 export default function ForgotPasswordButton({ ...props }) {
   const { openModal } = useModalStore(); // 넘겨야 하는 값 : options, content
+  const { options } = useModalStore();
+  const [email, setEmail] = useState<string>('');
+
+  useEffect(() => {
+    console.log('email', email);
+  }, [email]);
+
+  // 이메일 유효성 판단, 요청 보내고, toast 띄우기, 모달 닫기
+  const handleFormSubmit = () => {
+    console.log('부모 handleFormSubmit 실행'); //
+
+    const trimmedEmail = email.trim();
+    if (!trimmedEmail) return;
+
+    if (validateEmail(trimmedEmail)) {
+      options?.button?.onRequest({ trimmedEmail });
+      console.log('onRequest 호출');
+    }
+  };
 
   return (
     <div className="mt-3 mb-10 flex justify-end">
@@ -19,10 +46,19 @@ export default function ForgotPasswordButton({ ...props }) {
               button: {
                 number: 2,
                 text: '링크 보내기',
-                onRequest: (body) => console.log(body), // ?
+                // 모달 내부 폼에서 입력 받은 데이터를 외부로 전달해서 처리, 전역 상태로 저장
+                onRequest: (body) => {
+                  // 버튼 클릭 시 동작 함수
+                  // handleFormSubmit(body)
+                },
               },
-            },
-            <ResetPasswordLinkModal />
+            }, // options
+            // JSX 로 직접 렌더링?
+            <ResetPasswordLinkModal
+              value={email}
+              onValueChange={setEmail}
+              onFormSubmit={handleFormSubmit}
+            /> // content
           );
         }}
         {...props}
--- src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
+++ src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
@@ -1,9 +1,37 @@
+'use client';
+
 import InputBase from '@/components/common/Input/InputBase';
 
-export default function ResetPasswordLinkModal() {
+interface ResetPasswordLinkModalProps {
+  value: string;
+  onValueChange: (value: string) => void;
+  onFormSubmit: () => void; // handleFormSubmit
+}
+
+// content
+export default function ResetPasswordLinkModal({
+  value,
+  onValueChange,
+  onFormSubmit,
+}: ResetPasswordLinkModalProps) {
+  console.log('modal 렌더링됨');
   return (
-    <div>
-      <InputBase placeholder="이메일을 입력하세요." />
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault(); // form 이벤트 기본 동작은 자식이 처리
+        onFormSubmit(); // 부모 handleFormSubmit 호출
+        console.log('자식이 폼 제출');
+      }}
+    >
+      <InputBase
+        placeholder="이메일을 입력하세요."
+        value={value}
+        onChange={(e) => {
+          onValueChange(e.target.value);
+          console.log(e.target.value);
+        }}
+      />
+      <button type="submit">제출</button>
+    </form>
   );
 }
