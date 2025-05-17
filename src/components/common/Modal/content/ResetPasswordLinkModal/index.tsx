32ef02bf5e6ef0c65603a7b363bce452d9452dbc
해명 <hmyeonge@gmail.com>
2025-05-17 09:04:49 +0900

    feat: form 태그에 id 추가 / 폼 제출 시 requestBody 설정

--- src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
+++ src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
@@ -1,9 +1,41 @@
-import InputBase from '@/components/common/Input/InputBase';
+'use client';
 
+import InputBase from '@/components/common/Input/InputBase';
+import { useModalStore } from '@/store/useModalStore';
+import { validateEmail } from '@/utils/inputValidation';
+import { useRef } from 'react';
+
+// content
 export default function ResetPasswordLinkModal() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { setRequestBody } = useModalStore();
+
+  // 이메일 유효성 판단, 요청 보내고, toast 띄우기, 모달 닫기
+  const handleFormSubmit = () => {
+    const email = inputRef.current?.value.trim();
+    console.log('email', email); // x
+
+    if (!email) return;
+
+    if (validateEmail(email)) {
+      setRequestBody({ email });
+      console.log('if'); // x
+    }
+  };
+
   return (
-    <div>
-      <InputBase placeholder="이메일을 입력하세요." />
-    </div>
+    <form
+      id="reset-password-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        handleFormSubmit();
+      }}
+    >
+      <InputBase
+        ref={inputRef}
+        placeholder="이메일을 입력하세요."
+        defaultValue="" // 초기값 설정
+      />
+    </form>
   );
 }
