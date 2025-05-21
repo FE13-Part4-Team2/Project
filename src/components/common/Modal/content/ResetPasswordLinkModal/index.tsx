372717bb3ed3044f3d5e0f4687d23bcd597c6bb9
해명 <hmyeonge@gmail.com>
2025-05-21 11:04:50 +0900

    refactor: 유효성 검사 함수 이동에 따라 onSubmit 내부에서 해당 함수 호출 삭제

--- src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
+++ src/components/common/Modal/content/ResetPasswordLinkModal/index.tsx
@@ -1,31 +1,16 @@
 'use client';
 
 import InputBase from '@/components/common/Input/InputBase';
-import { useModalStore } from '@/store/useModalStore';
-import { validateEmail } from '@/utils/inputValidation';
 import { useRef } from 'react';
 
-// contents
 export default function ResetPasswordLinkModal() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const { setRequestBody } = useModalStore();
-
-  const handleEmailValidation = () => {
-    const email = inputRef.current?.value.trim();
-
-    if (!email) return;
-
-    if (validateEmail(email)) {
-      setRequestBody({ email });
-    }
-  };
 
   return (
     <form
       id="reset-password-form"
       onSubmit={(e) => {
         e.preventDefault();
-        handleEmailValidation();
       }}
     >
       <InputBase
