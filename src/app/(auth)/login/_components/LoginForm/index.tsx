395f1a8025b210203c381538b83b791fdd3ad199
해명 <hmyeonge@gmail.com>
2025-05-21 20:33:02 +0900

    refactor: OpenPasswordResetModal 로 컴포넌트명 변경에 따른 import 문 수정

--- src/app/(auth)/login/_components/LoginForm/index.tsx
+++ src/app/(auth)/login/_components/LoginForm/index.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import PasswordResetRequestModal from '@/app/(auth)/login/_components/LoginForm/PasswordResetRequestModal';
 import InputWithLabel from '@/components/auth/InputWithLabel';
 import Button from '@/components/common/Button';
 import { signIn } from '@/lib/apis/auth';
@@ -17,6 +16,7 @@ import Cookies from 'js-cookie';
 import { z } from 'zod';
 import { useRouter } from 'next/navigation';
 import { InputType } from '@/components/auth/type';
+import OpenPasswordResetModal from '@/app/(auth)/login/_components/LoginForm/OpenPasswordResetModal';
 
 // schema
 const inputEmptySchema = z.object({
@@ -205,7 +205,7 @@ export default function LoginForm() {
         />
       </div>
 
-      <PasswordResetRequestModal />
+      <OpenPasswordResetModal />
 
       <Button
         size="lg"
