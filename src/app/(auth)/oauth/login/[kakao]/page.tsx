b6961045eaf73fa794763fbccfcb71b708ab9b93
해명 <hmyeonge@gmail.com>
2025-05-23 10:57:53 +0900

    feat(KakaoLoginPage): 컴포넌트명 수정 및 searchParams 출력

--- src/app/(auth)/oauth/login/[kakao]/page.tsx
+++ src/app/(auth)/oauth/login/[kakao]/page.tsx
@@ -1,3 +1,11 @@
-export default function LoginPage() {
+export default function KakaoLoginPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  // code, state 를 추출해서 POST 요청
+  console.log('searchParams', searchParams);
+  // const { code, state } = searchParams;
+
   return <div>LoginPage</div>;
 }
